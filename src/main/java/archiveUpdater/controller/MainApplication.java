75453c84214ba700812d63f1563085b0c346f83f
mclee3360 <mclee3360@gmail.com>
2017-01-03 23:37:21 -0500

    Create application file

---
+++ src/main/java/archiveUpdater/controller/MainApplication.java
@@ -0,0 +1,19 @@
+package archiveUpdater.controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+/**
+ * Main Controller for the application.
+ *
+ * Created 01/03/2017
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
