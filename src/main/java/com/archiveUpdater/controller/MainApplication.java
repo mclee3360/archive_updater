b7537164e49aad323e42c041a003b904a1c6844a
mclee3360 <mclee3360@gmail.com>
2017-01-04 00:28:57 -0500

    Initialize firebase in application

---
+++ src/main/java/com/archiveUpdater/controller/MainApplication.java
@@ -0,0 +1,42 @@
+package archiveUpdater.controller;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ * Main Controller for the application.
+ *
+ * Created 01/03/2017
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        initDatabase();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void initDatabase() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                .setServiceAccount(new FileInputStream("rewrite-archive-f7a8b-fireb"
+                    + "ase-adminsdk-fqc5u-5d9e7b2d00.json"))
+                .setDatabaseUrl("https://rewrite-archive-f7a8b.firebaseio.com/")
+                .build();
+            FirebaseApp.initializeApp(options);
+        }
+        catch (java.io.FileNotFoundException e) {
+            Logger.getLogger(MainApplication.class.getName()).log(Level.SEVERE,
+                null, e);
+        }
+    }
+}
